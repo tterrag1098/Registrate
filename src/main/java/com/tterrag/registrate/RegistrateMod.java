85678d07cd3bb7cea5500309cc5f635b5941e702
tterrag <tterrag1098@gmail.com>
2019-12-05 17:08:19 -0500

    Add dummy mod for userdev

---
+++ src/main/java/com/tterrag/registrate/RegistrateMod.java
@@ -0,0 +1,9 @@
+package com.tterrag.registrate;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * This is a dummy mod that forces the Registrate jar to be loaded in the correct classloader in dev. When shading Registrate, make sure to remove this class and mods.toml.
+ */
+@Mod("registrate")
+public class RegistrateMod {}
