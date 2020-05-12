9c6be94f798ceea51b41538a5c35d071627f017f
tterrag <tterrag1098@gmail.com>
2020-05-11 22:20:43 -0400

    There there javac, it's okay, let me hold your hand

--- src/main/java/com/tterrag/registrate/builders/ContainerBuilder.java
+++ src/main/java/com/tterrag/registrate/builders/ContainerBuilder.java
@@ -38,7 +38,7 @@ public class ContainerBuilder<T extends Container, S extends Screen & IHasContai
     @Override
     protected @NonnullType ContainerType<T> createEntry() {
         ContainerType<T> ret = new ContainerType<>((windowId, inv) -> factory.create(get(), windowId, inv));
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> ScreenManager.registerFactory(ret, (type, inv, displayName) -> screenFactory.get().create(type, inv, displayName)));
+        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> ScreenManager.<T, S>registerFactory(ret, (type, inv, displayName) -> screenFactory.get().create(type, inv, displayName)));
         return ret;
     }
 }
