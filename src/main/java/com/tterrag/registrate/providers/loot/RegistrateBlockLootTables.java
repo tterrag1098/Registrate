881c5760333d255e21233b581d9ed99217280013
Apex <robsonlawson3@gmail.com>
2022-12-09 06:35:11 +0000

    Run the Protected Method & Source code data generators

--- src/main/java/com/tterrag/registrate/providers/loot/RegistrateBlockLootTables.java
+++ src/main/java/com/tterrag/registrate/providers/loot/RegistrateBlockLootTables.java
@@ -6,11 +6,9 @@ import lombok.RequiredArgsConstructor;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.data.loot.packs.VanillaBlockLoot;
-import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
 import net.minecraft.world.level.storage.loot.functions.FunctionUserBuilder;
@@ -40,97 +38,120 @@ public class RegistrateBlockLootTables extends VanillaBlockLoot implements Regis
     // @formatter:off
     // GENERATED START
 
-    public <T extends FunctionUserBuilder<T>> T applyExplosionDecay(ItemLike p_236222_, FunctionUserBuilder<T> p_236223_) { return super.applyExplosionDecay(p_236222_, p_236223_); }
+    @Override
+    public <T extends FunctionUserBuilder<T>> T applyExplosionDecay(ItemLike p_248695_, FunctionUserBuilder<T> p_248548_) { return super.applyExplosionDecay(p_248695_, p_248548_); }
 
-    public <T extends ConditionUserBuilder<T>> T applyExplosionCondition(ItemLike p_236225_, ConditionUserBuilder<T> p_236226_) { return super.applyExplosionCondition(p_236225_, p_236226_); }
+    @Override
+    public <T extends ConditionUserBuilder<T>> T applyExplosionCondition(ItemLike p_249717_, ConditionUserBuilder<T> p_248851_) { return super.applyExplosionCondition(p_249717_, p_248851_); }
 
-    public LootTable.Builder createSingleItemTable(ItemLike p_124127_) { return super.createSingleItemTable(p_124127_); }
+    public static LootTable.Builder createSelfDropDispatchTable(Block p_252253_, LootItemCondition.Builder p_248764_, LootPoolEntryContainer.Builder<?> p_249146_) { return BlockLootSubProvider.createSelfDropDispatchTable(p_252253_, p_248764_, p_249146_); }
 
-    public static LootTable.Builder createSelfDropDispatchTable(Block p_124172_, LootItemCondition.Builder p_124173_, LootPoolEntryContainer.Builder<?> p_124174_) { return BlockLootSubProvider.createSelfDropDispatchTable(p_124172_, p_124173_, p_124174_); }
+    public static LootTable.Builder createSilkTouchDispatchTable(Block p_250203_, LootPoolEntryContainer.Builder<?> p_252089_) { return BlockLootSubProvider.createSilkTouchDispatchTable(p_250203_, p_252089_); }
 
-    public static LootTable.Builder createSilkTouchDispatchTable(Block p_124169_, LootPoolEntryContainer.Builder<?> p_124170_) { return BlockLootSubProvider.createSilkTouchDispatchTable(p_124169_, p_124170_); }
+    public static LootTable.Builder createShearsDispatchTable(Block p_252195_, LootPoolEntryContainer.Builder<?> p_250102_) { return BlockLootSubProvider.createShearsDispatchTable(p_252195_, p_250102_); }
 
-    public static LootTable.Builder createShearsDispatchTable(Block p_124268_, LootPoolEntryContainer.Builder<?> p_124269_) { return BlockLootSubProvider.createShearsDispatchTable(p_124268_, p_124269_); }
+    public static LootTable.Builder createSilkTouchOrShearsDispatchTable(Block p_250539_, LootPoolEntryContainer.Builder<?> p_251459_) { return BlockLootSubProvider.createSilkTouchOrShearsDispatchTable(p_250539_, p_251459_); }
 
-    public static LootTable.Builder createSilkTouchOrShearsDispatchTable(Block p_124284_, LootPoolEntryContainer.Builder<?> p_124285_) { return BlockLootSubProvider.createSilkTouchOrShearsDispatchTable(p_124284_, p_124285_); }
+    @Override
+    public LootTable.Builder createSingleItemTableWithSilkTouch(Block p_249305_, ItemLike p_251905_) { return super.createSingleItemTableWithSilkTouch(p_249305_, p_251905_); }
 
-    public LootTable.Builder createSingleItemTableWithSilkTouch(Block p_124258_, ItemLike p_124259_) { return super.createSingleItemTableWithSilkTouch(p_124258_, p_124259_); }
+    @Override
+    public LootTable.Builder createSingleItemTable(ItemLike p_251584_, NumberProvider p_249865_) { return super.createSingleItemTable(p_251584_, p_249865_); }
 
-    public LootTable.Builder createSingleItemTable(ItemLike p_176040_, NumberProvider p_176041_) { return super.createSingleItemTable(p_176040_, p_176041_); }
+    @Override
+    public LootTable.Builder createSingleItemTableWithSilkTouch(Block p_251449_, ItemLike p_248558_, NumberProvider p_250047_) { return super.createSingleItemTableWithSilkTouch(p_251449_, p_248558_, p_250047_); }
 
-    public LootTable.Builder createSingleItemTableWithSilkTouch(Block p_176043_, ItemLike p_176044_, NumberProvider p_176045_) { return super.createSingleItemTableWithSilkTouch(p_176043_, p_176044_, p_176045_); }
+    public static LootTable.Builder createSilkTouchOnlyTable(ItemLike p_252216_) { return BlockLootSubProvider.createSilkTouchOnlyTable(p_252216_); }
 
-    public static LootTable.Builder createSilkTouchOnlyTable(ItemLike p_124251_) { return BlockLootSubProvider.createSilkTouchOnlyTable(p_124251_); }
+    @Override
+    public LootTable.Builder createPotFlowerItemTable(ItemLike p_249395_) { return super.createPotFlowerItemTable(p_249395_); }
 
-    public LootTable.Builder createPotFlowerItemTable(ItemLike p_124271_) { return super.createPotFlowerItemTable(p_124271_); }
+    @Override
+    public LootTable.Builder createSlabItemTable(Block p_251313_) { return super.createSlabItemTable(p_251313_); }
 
-    public LootTable.Builder createSlabItemTable(Block p_124291_) { return super.createSlabItemTable(p_124291_); }
+    @Override
+    public LootTable.Builder createNameableBlockEntityTable(Block p_252291_) { return super.createNameableBlockEntityTable(p_252291_); }
 
-    public LootTable.Builder createNameableBlockEntityTable(Block p_124293_) { return super.createNameableBlockEntityTable(p_124293_); }
+    @Override
+    public LootTable.Builder createShulkerBoxDrop(Block p_252164_) { return super.createShulkerBoxDrop(p_252164_); }
 
-    public LootTable.Builder createShulkerBoxDrop(Block p_124295_) { return super.createShulkerBoxDrop(p_124295_); }
+    @Override
+    public LootTable.Builder createCopperOreDrops(Block p_251306_) { return super.createCopperOreDrops(p_251306_); }
 
-    public LootTable.Builder createCopperOreDrops(Block p_176047_) { return super.createCopperOreDrops(p_176047_); }
+    @Override
+    public LootTable.Builder createLapisOreDrops(Block p_251511_) { return super.createLapisOreDrops(p_251511_); }
 
-    public LootTable.Builder createLapisOreDrops(Block p_176049_) { return super.createLapisOreDrops(p_176049_); }
+    @Override
+    public LootTable.Builder createRedstoneOreDrops(Block p_251906_) { return super.createRedstoneOreDrops(p_251906_); }
 
-    public LootTable.Builder createRedstoneOreDrops(Block p_176051_) { return super.createRedstoneOreDrops(p_176051_); }
+    @Override
+    public LootTable.Builder createBannerDrop(Block p_249810_) { return super.createBannerDrop(p_249810_); }
 
-    public LootTable.Builder createBannerDrop(Block p_124297_) { return super.createBannerDrop(p_124297_); }
+    public static LootTable.Builder createBeeNestDrop(Block p_250988_) { return BlockLootSubProvider.createBeeNestDrop(p_250988_); }
 
-    public static LootTable.Builder createBeeNestDrop(Block p_124299_) { return BlockLootSubProvider.createBeeNestDrop(p_124299_); }
+    public static LootTable.Builder createBeeHiveDrop(Block p_248770_) { return BlockLootSubProvider.createBeeHiveDrop(p_248770_); }
 
-    public static LootTable.Builder createBeeHiveDrop(Block p_124301_) { return BlockLootSubProvider.createBeeHiveDrop(p_124301_); }
+    public static LootTable.Builder createCaveVinesDrop(Block p_251070_) { return BlockLootSubProvider.createCaveVinesDrop(p_251070_); }
 
-    public static LootTable.Builder createCaveVinesDrop(Block p_176053_) { return BlockLootSubProvider.createCaveVinesDrop(p_176053_); }
+    @Override
+    public LootTable.Builder createOreDrop(Block p_250450_, Item p_249745_) { return super.createOreDrop(p_250450_, p_249745_); }
 
-    public LootTable.Builder createOreDrop(Block p_124140_, Item p_124141_) { return super.createOreDrop(p_124140_, p_124141_); }
+    @Override
+    public LootTable.Builder createMushroomBlockDrop(Block p_249959_, ItemLike p_249315_) { return super.createMushroomBlockDrop(p_249959_, p_249315_); }
 
-    public LootTable.Builder createMushroomBlockDrop(Block p_124278_, ItemLike p_124279_) { return super.createMushroomBlockDrop(p_124278_, p_124279_); }
+    @Override
+    public LootTable.Builder createGrassDrops(Block p_252139_) { return super.createGrassDrops(p_252139_); }
 
-    public LootTable.Builder createGrassDrops(Block p_124303_) { return super.createGrassDrops(p_124303_); }
+    public static LootTable.Builder createShearsOnlyDrop(ItemLike p_250684_) { return BlockLootSubProvider.createShearsOnlyDrop(p_250684_); }
 
-    public LootTable.Builder createStemDrops(Block p_124255_, Item p_124256_) { return super.createStemDrops(p_124255_, p_124256_); }
+    @Override
+    public LootTable.Builder createMultifaceBlockDrops(Block p_249088_, LootItemCondition.Builder p_251535_) { return super.createMultifaceBlockDrops(p_249088_, p_251535_); }
 
-    public LootTable.Builder createAttachedStemDrops(Block p_124275_, Item p_124276_) { return super.createAttachedStemDrops(p_124275_, p_124276_); }
+    @Override
+    public LootTable.Builder createLeavesDrops(Block p_250088_, Block p_250731_, float... p_248949_) { return super.createLeavesDrops(p_250088_, p_250731_, p_248949_); }
 
-    public static LootTable.Builder createShearsOnlyDrop(ItemLike p_124287_) { return BlockLootSubProvider.createShearsOnlyDrop(p_124287_); }
+    @Override
+    public LootTable.Builder createOakLeavesDrops(Block p_249535_, Block p_251505_, float... p_250753_) { return super.createOakLeavesDrops(p_249535_, p_251505_, p_250753_); }
 
-    public LootTable.Builder createMultifaceBlockDrops(Block p_236228_, LootItemCondition.Builder p_236229_) { return super.createMultifaceBlockDrops(p_236228_, p_236229_); }
+    @Override
+    public LootTable.Builder createMangroveLeavesDrops(Block p_251103_) { return super.createMangroveLeavesDrops(p_251103_); }
 
-    public LootTable.Builder createLeavesDrops(Block p_124158_, Block p_124159_, float... p_124160_) { return super.createLeavesDrops(p_124158_, p_124159_, p_124160_); }
+    @Override
+    public LootTable.Builder createCropDrops(Block p_249457_, Item p_248599_, Item p_251915_, LootItemCondition.Builder p_252202_) { return super.createCropDrops(p_249457_, p_248599_, p_251915_, p_252202_); }
 
-    public LootTable.Builder createOakLeavesDrops(Block p_124264_, Block p_124265_, float... p_124266_) { return super.createOakLeavesDrops(p_124264_, p_124265_, p_124266_); }
+    public static LootTable.Builder createDoublePlantShearsDrop(Block p_248678_) { return BlockLootSubProvider.createDoublePlantShearsDrop(p_248678_); }
 
-    public LootTable.Builder createMangroveLeavesDrops(Block p_236249_) { return super.createMangroveLeavesDrops(p_236249_); }
+    @Override
+    public LootTable.Builder createDoublePlantWithSeedDrops(Block p_248590_, Block p_248735_) { return super.createDoublePlantWithSeedDrops(p_248590_, p_248735_); }
 
-    public LootTable.Builder createCropDrops(Block p_124143_, Item p_124144_, Item p_124145_, LootItemCondition.Builder p_124146_) { return super.createCropDrops(p_124143_, p_124144_, p_124145_, p_124146_); }
+    @Override
+    public LootTable.Builder createCandleDrops(Block p_250896_) { return super.createCandleDrops(p_250896_); }
 
-    public static LootTable.Builder createDoublePlantShearsDrop(Block p_124305_) { return BlockLootSubProvider.createDoublePlantShearsDrop(p_124305_); }
+    public static LootTable.Builder createCandleCakeDrops(Block p_250280_) { return BlockLootSubProvider.createCandleCakeDrops(p_250280_); }
 
-    public LootTable.Builder createDoublePlantWithSeedDrops(Block p_124261_, Block p_124262_) { return super.createDoublePlantWithSeedDrops(p_124261_, p_124262_); }
+    @Override
+    public void addNetherVinesDropTable(Block p_252269_, Block p_250696_) { super.addNetherVinesDropTable(p_252269_, p_250696_); }
 
-    public LootTable.Builder createCandleDrops(Block p_176057_) { return super.createCandleDrops(p_176057_); }
+    @Override
+    public LootTable.Builder createDoorTable(Block p_252166_) { return super.createDoorTable(p_252166_); }
 
-    public static LootTable.Builder createCandleCakeDrops(Block p_176059_) { return BlockLootSubProvider.createCandleCakeDrops(p_176059_); }
+    @Override
+    public void dropPottedContents(Block p_251064_) { super.dropPottedContents(p_251064_); }
 
-    @Override public void dropSelf(Block p_249181_) { super.dropSelf(p_249181_); }
+    @Override
+    public void otherWhenSilkTouch(Block p_249932_, Block p_252053_) { super.otherWhenSilkTouch(p_249932_, p_252053_); }
 
-    @Override public void add(Block p_124166_, LootTable.Builder p_124167_) { super.add(p_124166_, p_124167_); }
+    @Override
+    public void dropOther(Block p_248885_, ItemLike p_251883_) { super.dropOther(p_248885_, p_251883_); }
 
-    @Override public<T extends Comparable<T>&StringRepresentable> LootTable.Builder createSinglePropConditionTable(Block p_252154_, Property<T> p_250272_, T p_250292_) { return super.createSinglePropConditionTable(p_252154_,p_250272_,p_250292_); }
+    @Override
+    public void dropWhenSilkTouch(Block p_250855_) { super.dropWhenSilkTouch(p_250855_); }
 
-    @Override public void addNetherVinesDropTable(Block p_252269_, Block p_250696_) { super.addNetherVinesDropTable(p_252269_,p_250696_); }
+    @Override
+    public void dropSelf(Block p_249181_) { super.dropSelf(p_249181_); }
 
-    @Override public LootTable.Builder createDoorTable(Block p_252166_) { return super.createDoorTable(p_252166_); }
-
-    @Override public void dropPottedContents(Block p_251064_) { super.dropPottedContents(p_251064_); }
-
-    @Override public void otherWhenSilkTouch(Block p_249932_, Block p_252053_) { super.otherWhenSilkTouch(p_249932_,p_252053_); }
-
-    @Override public void dropOther(Block p_248885_, ItemLike p_251883_) { super.dropOther(p_248885_,p_251883_); }
-
-    @Override public void dropWhenSilkTouch(Block p_250855_) { super.dropWhenSilkTouch(p_250855_); }
+    @Override
+    public void add(Block p_250610_, LootTable.Builder p_249817_) { super.add(p_250610_, p_249817_); }
 
     // GENERATED END
 }
--- src/main/java/com/tterrag/registrate/providers/loot/RegistrateEntityLootTables.java
+++ src/main/java/com/tterrag/registrate/providers/loot/RegistrateEntityLootTables.java
@@ -4,10 +4,14 @@ import com.tterrag.registrate.AbstractRegistrate;
 import lombok.RequiredArgsConstructor;
 
 import net.minecraft.core.registries.Registries;
+import net.minecraft.data.loot.EntityLootSubProvider;
 import net.minecraft.data.loot.packs.VanillaEntityLoot;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.FrogVariant;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 
 import java.util.function.Consumer;
 import java.util.function.Supplier;
@@ -32,11 +36,19 @@ public class RegistrateEntityLootTables extends VanillaEntityLoot implements Reg
     // @formatter:off
     // GENERATED START
 
-    @Override
-    public void add(EntityType<?> p_124372_, LootTable.Builder p_124373_) { super.add(p_124372_, p_124373_); }
+    public static LootTable.Builder createSheepTable(ItemLike p_249422_) { return EntityLootSubProvider.createSheepTable(p_249422_); }
 
     @Override
-    public void add(EntityType<?> entityType, ResourceLocation p_124381_, LootTable.Builder p_124382_) { super.add(entityType, p_124381_, p_124382_); }
+    public boolean canHaveLootTable(EntityType<?> p_249029_) { return super.canHaveLootTable(p_249029_); }
+
+    @Override
+    public LootItemCondition.Builder killedByFrogVariant(FrogVariant p_249403_) { return super.killedByFrogVariant(p_249403_); }
+
+    @Override
+    public void add(EntityType<?> p_248740_, LootTable.Builder p_249440_) { super.add(p_248740_, p_249440_); }
+
+    @Override
+    public void add(EntityType<?> p_252130_, ResourceLocation p_251706_, LootTable.Builder p_249357_) { super.add(p_252130_, p_251706_, p_249357_); }
 
     // GENERATED END
 }
