ab3ba5486a5672ccf13d43b21cceb193534c0161
Apex <29412632+ApexModder@users.noreply.github.com>
2023-03-18 04:54:19 +0000

    1.19.4 Update (#53)
    * Fix issue where creative tab modification order was not preserved, causing items to display in random slots on creative tabs.
    
    * Initial port to 1.19.4
    
    * Implement intrinsic variant for tag providers
    Fixes tag providers not being able to reference objects of same type
    `provider.tag(MyModItemTags.MY_ITEM_TAG).add(MyModItems.MY_ITEM.get())`
    
    Update item tag provider to make use for completable future much like how vanilla does now
    
    Update block, fluid, item & entity type tag providers to now be intrinsic provider types
    
    * Fix typo in comment
    
    * Fix RegistrateIntrinsicHolderTagsProvider not extending vanilla class by introducing an interface based system for tag providers
    
    * Remove unneeded getBuilder method

--- src/main/java/com/tterrag/registrate/providers/RegistrateTagsProvider.java
+++ src/main/java/com/tterrag/registrate/providers/RegistrateTagsProvider.java
@@ -1,49 +1,87 @@
 package com.tterrag.registrate.providers;
 
 import com.tterrag.registrate.AbstractRegistrate;
-
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.Registry;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.IntrinsicHolderTagsProvider;
 import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.tags.TagBuilder;
 import net.minecraft.tags.TagKey;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.fml.LogicalSide;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
-public class RegistrateTagsProvider<T> extends TagsProvider<T> implements RegistrateProvider {
+public interface RegistrateTagsProvider<T> extends RegistrateProvider {
+    TagsProvider.TagAppender<T> addTag(TagKey<T> tag);
 
-    private final AbstractRegistrate<?> owner;
-    private final ProviderType<? extends RegistrateTagsProvider<T>> type;
-    private final String name;
+    class Impl<T> extends TagsProvider<T> implements RegistrateTagsProvider<T> {
+        private final AbstractRegistrate<?> owner;
+        private final ProviderType<? extends Impl<T>> type;
+        private final String name;
 
-    public RegistrateTagsProvider(AbstractRegistrate<?> owner, ProviderType<? extends RegistrateTagsProvider<T>> type, String name, PackOutput packOutput, ResourceKey<? extends Registry<T>> registryIn, CompletableFuture<HolderLookup.Provider> registriesLookup, ExistingFileHelper existingFileHelper) {
-        super(packOutput, registryIn, registriesLookup, owner.getModid(), existingFileHelper);
-        this.owner = owner;
-        this.type = type;
-        this.name = name;
+        public Impl(AbstractRegistrate<?> owner, ProviderType<? extends Impl<T>> type, String name, PackOutput packOutput, ResourceKey<? extends Registry<T>> registryIn, CompletableFuture<HolderLookup.Provider> registriesLookup, ExistingFileHelper existingFileHelper) {
+            super(packOutput, registryIn, registriesLookup, owner.getModid(), existingFileHelper);
+
+            this.owner = owner;
+            this.type = type;
+            this.name = name;
+        }
+
+        @Override
+        public String getName() {
+            return "Tags (%s)".formatted(name);
+        }
+
+        @Override
+        protected void addTags(HolderLookup.Provider provider) {
+            owner.genData(type, this);
+        }
+
+        @Override
+        public LogicalSide getSide() {
+            return LogicalSide.SERVER;
+        }
+
+        @Override
+        public TagAppender<T> addTag(TagKey<T> tag) {
+            return super.tag(tag);
+        }
     }
 
-    public String getName() {
-        return "Tags (" + name + ")";
+    class IntrinsicImpl<T> extends IntrinsicHolderTagsProvider<T> implements RegistrateTagsProvider<T> {
+        private final AbstractRegistrate<?> owner;
+        private final ProviderType<? extends IntrinsicImpl<T>> type;
+        private final String name;
+
+        public IntrinsicImpl(AbstractRegistrate<?> owner, ProviderType<? extends IntrinsicImpl<T>> type, String name, PackOutput packOutput, ResourceKey<? extends Registry<T>> registryIn, CompletableFuture<HolderLookup.Provider> registriesLookup, Function<T, ResourceKey<T>> keyExtractor, ExistingFileHelper existingFileHelper) {
+            super(packOutput, registryIn, registriesLookup, keyExtractor, owner.getModid(), existingFileHelper);
+
+            this.owner = owner;
+            this.type = type;
+            this.name = name;
+        }
+
+        @Override
+        public String getName() {
+            return "Tags (%s)".formatted(name);
+        }
+
+        @Override
+        protected void addTags(HolderLookup.Provider provider) {
+            owner.genData(type, this);
+        }
+
+        @Override
+        public LogicalSide getSide() {
+            return LogicalSide.SERVER;
+        }
+
+        @Override
+        public IntrinsicTagAppender<T> addTag(TagKey<T> tag) {
+            return super.tag(tag);
+        }
     }
-
-    @Override
-    protected void addTags(HolderLookup.Provider provider) {
-        owner.genData(type, this);
-    }
-
-    @Override
-    public LogicalSide getSide() {
-        return LogicalSide.SERVER;
-    }
-
-    @Override
-    public TagAppender<T> tag(TagKey<T> tag) { return super.tag(tag); }
-
-    @Override
-    public TagBuilder getOrCreateRawBuilder(TagKey<T> tag) { return super.getOrCreateRawBuilder(tag); }
 }
