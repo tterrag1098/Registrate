1202ba3015f4fe88a26abd0e69e43bf77177d999
lcy0x1 <chengyilin@umail.ucsb.edu>
2025-03-25 19:55:48 +0800

    block entity port

--- src/main/java/com/tterrag/registrate/builders/BlockEntityBuilder.java
+++ src/main/java/com/tterrag/registrate/builders/BlockEntityBuilder.java
@@ -136,8 +136,7 @@ public class BlockEntityBuilder<T extends BlockEntity, P> extends AbstractBuilde
     protected BlockEntityType<T> createEntry() {
         BlockEntityFactory<T> factory = this.factory;
         final var supplier = asSupplier();
-        return BlockEntityType.Builder.of((pos, state) -> factory.create(supplier.get(), pos, state), validBlocks.stream().map(NonNullSupplier::get).toArray(Block[]::new))
-                .build(null);
+        return new BlockEntityType<>((pos, state) -> factory.create(supplier.get(), pos, state), validBlocks.stream().map(NonNullSupplier::get).toArray(Block[]::new));
     }
 
     @Override
